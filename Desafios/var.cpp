5985ff23340c5468e746c8e3680ec313d4111c74
FabricioRubim <fabriciofraga200618@gmail.com>
2025-10-20 18:49:16 -0300

    questão de variavel

---
+++ Desafios/var.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string nome;
+    int idade;
+
+    cout << "Digite seu nome\n";
+    cin >> nome;
+
+    cout << "Digite sua idade\n";
+    cin >> idade;
+
+    cout << "Seu nome é: \n" << nome << endl;
+
+    cout << "\n";
+
+    cout << "Sua idade é: " << idade << endl;
+
+    return 0;
+}
