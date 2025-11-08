3e200b7a29c96d9d67fbb431d2cd2a111552b5e5
FabricioRubim <fabriciofraga200618@gmail.com>
2025-11-08 16:19:50 -0300

    alterações no codigo

--- Desafios/index.cpp
+++ Desafios/index.cpp
@@ -1,5 +1,14 @@
 #include <bits/stdc++.h>
-
+// movimento da camera
+    int cam_movementX = 2;
+    int cam_movementY = -2;
+// movimento do player
+    int player_movementX = 2;
+    int player_movementY = -2;
+// player hp
+    int playerHP = 20;
+// Player damage
+    int playerDamage = 5;
 int main(){
     
     return 0;
