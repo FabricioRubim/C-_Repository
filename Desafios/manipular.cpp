5900e9218555fb9e2ad71299af3412e5b81aad9f
FabricioRubim <fabriciofraga200618@gmail.com>
2025-11-08 16:07:08 -0300

    first commit

---
+++ Desafios/manipular.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct pessoa
+{
+    string nome;
+    int idade;
+};
+
+int main(){
+    pessoa joao = {"João", 20};
+    cout << joao.nome << endl;
+    cout << joao.idade << endl;
+    return 0;
+}
