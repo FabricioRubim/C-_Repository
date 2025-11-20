75d19e47770b2a9121ee1399620c716eb7fc4d1e
FabricioRubim <fabriciofraga200618@gmail.com>
2025-11-20 18:32:38 -0300

    index

--- Desafios/index.cpp
+++ Desafios/index.cpp
@@ -10,6 +10,27 @@
 // Player damage
     int playerDamage = 5;
 int main(){
-    
+    std::string enemy = "Zombie";
+    int zombieHP = 10;
+    int damageZombie = 2;
+
+    if(playerHP && damageZombie){
+        playerHP -= 2;
+        playerHP;
+        std::cout  << "HP do player " << playerHP << std::endl;
+    }
+    else{
+        std::cout << "Ataque bloqueado";
+    }
+
+    if(playerDamage && zombieHP)
+    {
+        zombieHP -= 2;
+        zombieHP;
+        std::cout << "HP do zombie " << zombieHP << std::endl;
+    }
+    else{
+        std::cout << "Zombie desviou";
+    }
     return 0;
 }
