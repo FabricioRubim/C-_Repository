ffbf34097bfb294492ebe88fd900f6177edacb50
FabricioRubim <fabriciofraga200618@gmail.com>
2026-04-05 16:30:38 -0300

    fix(src): Arrumando o codigo

--- Desafios/src/imc.cpp
+++ Desafios/src/imc.cpp
@@ -1,30 +1,41 @@
 #include <iostream>
+#include <iomanip>
 #include <math.h>
-using namespace std;
 
-int p = 40;
-int a = 1.85;
-int result = p / pow(a, a);
+
+double calculo_imc(){
+double p;
+double a;
+std::cout << "Digite o seu peso: " << "\n";
+std::cin >> p;
+std::cout << "Digite sua altura: " << "\n";
+std::cin >> a;
+std::cout << std::fixed << std::setprecision(1);
+double result = (p) / pow(a, 2);
+return result;
+}
 
 int main(){
 
+    double result = calculo_imc();
+
     if(result < 18.5){
-        cout << "Abaixo do peso " << result;
+        std::cout << "Abaixo do peso " << result;
     }
     else if(result >= 18.5 && result <= 24.99){
-        cout << "Peso normal " << result;
+        std::cout << "Peso normal " << result;
     }
     else if(result >= 25 && result <= 29.99){
-        cout << "Acima do peso " << result;
+        std::cout << "Acima do peso " << result;
     }
     else if(result >= 30 && result <= 34.99){
-        cout << "Obesidade grau I " << result;
+        std::cout << "Obesidade grau I " << result;
     }
     else if(result >= 35 && result <= 39.99){
-        cout << "Obesidade grau II (severa) " << result;
+        std::cout << "Obesidade grau II (severa) " << result;
     }
     else{
-        cout << "Obesidade grau III (mórbida) " << result;
+        std::cout << "Obesidade grau III (mórbida) " << result;
     }
 
     return 0;
--- Desafios/src/manipular.cpp
+++ Desafios/src/manipular.cpp
@@ -1,16 +1,13 @@
 #include <bits/stdc++.h>
-using namespace std;
 
 struct pessoa
 {
-    string nome;
+    std::string nome;
     int idade;
 };
 
 int main(){
     pessoa joao = {"João", 20};
-    cout << joao.nome << endl;
-    cout << joao.idade << endl;
-    cout << nome.len() << endl;
+    std::cout << "Você é " + joao.nome + ", e sua idade é " << joao.idade << "\n";
     return 0;
 }
